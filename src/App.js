67de07384f9da8a6129588d7596a41588e3b3d29
sunpark <yangheat@gmail.com>
2022-06-05 19:35:05 +0900

    useEffect section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
+
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  const onClick = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const hideEvent = () => {
+    setShowing((prev) => !prev);
+  };
+
+  console.log("i run all the time");
+
+  // Excute useEffect when first render
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+
+  // Excute useEffect when first render or count state chages
+  useEffect(() => {
+    console.log("I run when count changes.");
+  }, [count]);
+
+  // Excute useEffect when first render or keyword state changes
+  useEffect(() => {
+    console.log("I run when keyword changes.");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when count or keyword changes.");
+  }, [count, keyword]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text="Hello"></Button>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Serach here."
+      />
+      <h1 className={styles.title}>{count}</h1>
+      <button onClick={onClick}>click me</button>
+      <button onClick={hideEvent}>{showing ? "hide" : "show"}</button>
+      {showing ? <Button text="hello"></Button> : null}
     </div>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,12 @@
 import PropTypes from "prop-types";
+import { useEffect } from "react";
 import styles from "./Button.module.css";
 
 function Button({ text }) {
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("destroyed :(");
+  }, []);
   return <button className={styles.btn}>{text}</button>;
 }
 
