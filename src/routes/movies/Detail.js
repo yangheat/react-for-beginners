0be1b32f78ae3e75a1f0497fbd1e93f83fe66816
sunpark <yangheat@gmail.com>
2022-07-06 12:34:24 +0900

    Add useCallback function

--- src/routes/movies/Detail.js
+++ src/routes/movies/Detail.js
@@ -1,19 +1,20 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
   const id = useParams().id;
 
-  const getMovie = async () => {
-    const json = await fetch(
-      `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
-    ).then((response) => response.json());
-    console.log(json.data);
-  };
+  const getMovie = useCallback(async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+  }, [id]);
 
   useEffect(() => {
     getMovie();
-  }, []);
+  }, [getMovie]);
+
   return <div>Detail Page</div>;
 }
 
